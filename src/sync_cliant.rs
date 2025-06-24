4ced070a4921475024fbd40281e84545991d0650
val-en-tine124 <valentinechibueze400@gmail.com>
2025-06-24 21:02:56 +0100

    import dependencies.

--- src/sync_cliant.rs
+++ src/sync_cliant.rs
@@ -1,5 +1,12 @@
-use log;
+#![allow(unused)]
+use std::borrow::Cow;
+use std::{collections::HashMap, time::Duration};
 
-fn setup_log(){
-    
-}
+use crate::types::HttpClientConfig;
+use anyhow::{Context, Error, Result};
+use colored::Colorize;
+use rayon;
+use reqwest::blocking::{Client, ClientBuilder};
+use reqwest::cookie::Cookie;
+use reqwest::header::{HeaderMap, HeaderValue, CONTENT_DISPOSITION, CONTENT_TYPE, RANGE};
+use reqwest::{redirect::Policy, NoProxy, Proxy, Url};
