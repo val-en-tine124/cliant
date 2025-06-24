519f63972cc228306a0f352b9bf350c6a00a46e1
val-en-tine124 <valentinechibueze400@gmail.com>
2025-06-24 21:34:21 +0100

    Add function to  get download file extension from url.

--- src/sync_cliant.rs
+++ src/sync_cliant.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 use std::borrow::Cow;
+use std::rc::Rc;
 use std::{collections::HashMap, time::Duration};
 
 use crate::types::HttpClientConfig;
@@ -19,7 +20,7 @@ fn build_client(http_config: HttpClientConfig) -> Result<()> {
         Policy::none()
     };
 
-    // Configure request timeout.
+    // Configure request timeout default to sixty seconds.
     let timeout = Duration::new(http_config.timeout.unwrap_or(60), 0);
 
     let mut client_config = ClientBuilder::new().timeout(timeout).redirect(policy);
@@ -52,4 +53,74 @@ fn infer_file_ext(buf: &[u8]) -> Option<String> {
         return Some(inferred_type.extension().to_string());
     }
     None
+}
+
+fn get_file_extension(url: Url, client: Client) -> Result<String> {
+    //construct headermap.
+    let mut header_map = HeaderMap::new();
+    header_map.insert(
+        RANGE,
+        HeaderValue::from_str("bytes=0-2048")
+            .context("can't convert supplied bytes range to ASCII text")?,
+    );
+    //make http request to site.
+    let response = client
+        .get(url.as_ref())
+        .headers(header_map)
+        .send()
+        .context(format!(
+            "{} can't send request to target url.",
+            "Error:".red()
+        ))?;
+    // check for invalid status code.
+    let response_status = response.error_for_status_ref();
+    if let Err(err) = response_status {
+        eprintln!(
+            "{}",
+            format!(
+                "{} got status code {},it originated from url : {}",
+                "Error:".red(),
+                err.status().map_or_else(
+                    || { "No status".red() },
+                    |status| { status.as_str().blue() }
+                ),
+                err.url().unwrap_or(&url)
+            )
+        );
+    }
+
+    //Get content type for extension inference.
+    let content_type = response
+        .headers()
+        .get(CONTENT_TYPE)
+        .context(format!(
+            "{} Can't get CONTENT_TYPE from http response header.",
+            "Error:".red()
+        ))?
+        .clone();
+
+    //Get bytes for memory buffer bases type inference.
+    let bytes = response.bytes().context(format!(
+        "{} Can't get response body as bytes",
+        "Error:".red()
+    ))?;
+
+    let ext = infer_file_ext(&bytes);
+    if let Some(ext) = ext {
+        return Ok(ext);
+    } else {
+        let mut split_content_type = content_type
+            .to_str()
+            .context(format!(
+                "{} Can't covert content-type string to valid ASCII",
+                "Error:".red(),
+            ))?
+            .split("/")
+            .collect::<Vec<&str>>();
+        let ext = split_content_type
+            .pop()
+            .context(format!("{} Can't parse content type string.","Error:".red()))?
+            .trim_matches('"');
+        return Ok(ext.into());
+    }
 }
