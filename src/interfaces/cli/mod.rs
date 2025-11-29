ff7fb508c829543e2dca3c035f66116827a9283b
val-en-tine124 <valentinechibueze400@gmail.com>
2025-11-29 17:58:16 +0100

    chore:Create a clap parser instance with arguments  url,output,home_dir,timeout,max_no_retries,retry_delay_secs and a custom url parser

--- src/interfaces/cli/mod.rs
+++ src/interfaces/cli/mod.rs
@@ -1,1 +1,31 @@
+use clap::{Parser, arg, command,};
+use std::path::PathBuf;
+use url::Url;
 
+#[derive(Parser)]
+#[command(version,about,long_about=None)]
+pub struct Cliant {
+    #[arg(value_parser=parse_url)]
+    pub url: Vec<Url>,
+    #[arg(short, long)]
+    pub output: Option<PathBuf>,
+    #[arg(short = 'H', long)]
+    pub home_dir: Option<PathBuf>,
+    #[arg(short = 't', long)]
+    pub timeout: Option<usize>,
+    #[arg(short = 'r', long)]
+    pub max_no_retries: Option<usize>,
+    #[arg(short = 'd', long)]
+    pub retry_delay_secs: Option<usize>,
+
+}
+
+fn parse_url(url:&str)->Result<Url,String>{
+    if url.starts_with("https://") || url.starts_with("http://"){
+        let parsed_url=Url::parse(url).map_err(|e|format!("Invalid Url {url} {e}"));
+        return parsed_url;
+    }
+    let new_url=format!("https://{url}");
+    let parsed_url=Url::parse(&new_url).map_err(|e|format!("Invalid Url {url} {e}"));
+    parsed_url
+}
