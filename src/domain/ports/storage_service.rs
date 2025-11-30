d189311a688063ef5c59d953ecac0722de3da44f
val-en-tine124 <valentinechibueze400@gmail.com>
2025-11-30 15:33:59 +0100

    chore: Improve documentation of traits and their methods

--- src/domain/ports/storage_service.rs
+++ src/domain/ports/storage_service.rs
@@ -1,40 +1,41 @@
+//! This module contains traits defining common file system operations.
+//! These traits allows for abstracting file system interactions, making it easier
+//! to test and potentially swap out different file system implementations.
 use anyhow::Result;
 use std::path::Path;
 use futures::stream::Stream;
 use bytes::Bytes;
 use crate::domain::models::file_info::FileInfo;
 
-/// A trait defining common file system operations.
-///
-/// This trait allows for abstracting file system interactions, making it easier
-/// to test and potentially swap out different file system implementations.
+
+///Trait to seek and write to a file on a file system implementation.
 pub trait SeekAndWrite {
     
-    //Set a predefined file length.
+    /// Set a predefined file length.
     async fn set_len(&self,path:&Path,file_size:usize,)->Result<()>;
-    //Remember to add write_at method that will Seek to a position in a file and write a buffer to that location.
-
+    
+    ///This method will Seek to a position in a file and write a buffer to that location.
     async fn write_at(&self,path:&Path,pos:usize,buf:&[u8])->Result<()>;
 }
 
 trait ReadWrite{
-     //Read a file content to a stream.
+     /// Read a file content to a stream.
     fn read_file(&self,path:&Path)-> Result<impl Stream<Item=Result<Bytes
     >>>;
-    //Write a buffer content to a file truncating the file(overwrite previous content).
+    /// Write a buffer content to a file truncating the file(overwrite previous content).
     async fn write_file(&self,content:&[u8],path:&Path)->Result<()
     >;
 
-    //append a buffer content to a file.
+    /// Append a buffer content to a file.
     async fn append_to_file(&self,content:& [u8],path:&Path)->Result<()
     >;
 
 }
 
 trait FileStatus{
-    //check if a path exists on the file system.
+    /// Check if a path exists on the file system.
     async fn file_exists(&self,path:&Path)->Result<bool>;
-    //Get file information or metadata.
+    /// Get file information or metadata.
     async fn file_info<'a>(&'a self,path:&'a Path)->Result<FileInfo>;
 }
 trait CreateDelete{
@@ -57,7 +58,7 @@ pub trait DirIO{
     /// Recursively removes a directory and all its contents.
     async fn remove_dir_all(&self, path: &Path) -> Result<()
     >;
-    //check if a path exists on the file system.
+    /// Check if a path exists on the file system.
     async fn dir_exists(&self,path:&Path)->Result<bool
     >;
 }
