f06924d494d7db8866ebb8d481c130b46a05d661
val-en-tine124 <valentinechibueze400@gmail.com>
2025-12-21 10:59:44 +0100

    chire: Add cli setup to main  file

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@
 //! parses command-line arguments, configures the HTTP client, and starts the
 //! download process.
 
-use crate::interfaces::cli::Cliant;
-use clap::Parser;
+use crate::interfaces::cli::set_up_cli_app;
+use anyhow::Result;
 
 mod application;
 mod domain;
@@ -15,90 +15,10 @@ mod infra;
 mod interfaces;
 mod utils;
 
-// use clap::Parser;
-// use errors::CliantError;
-// use reqwest::Url;
-// use std::path::PathBuf;
-// use std::process::exit;
 
-// #[derive(Parser)]
-// #[command(author, version, about, long_about = None)]
-// struct Cli {
-//     #[arg(short, long)]
-//     url: Url,
-//     #[arg(short, long)]
-//     ///Path to save the file.
-//     path: Option<PathBuf>,
-//     #[arg(short, long, default_value_t = false)]
-//     verbose: bool,
-//     #[arg(short = 'U', long)]
-//     username: Option<String>,
-//     #[arg(short = 'P', long)]
-//     password: Option<String>,
-//     #[arg(long)]
-//     max_redirects: Option<usize>,
-//     #[arg(short = 't', long)]
-//     timeout: Option<u64>,
-//     #[arg(long)]
-//     proxy_url: Option<String>,
-
-//     /// A semi-column seperated key value pair e.g key1=value1;key2=value2.
-//     #[arg(short = 'H', long)]
-//     request_headers: Option<String>,
-
-//     #[arg(short = 'c', long)]
-//     http_cookies: Option<String>,
-//     #[arg(long)]
-//     http_version: Option<String>,
-//     #[arg(short = 'M', long)]
-//     max_concurrent_part: Option<u32>,
-// }
-
-// fn main() -> Result<(), CliantError> {
-//     let cli = Cli::parse();
-
-//     if cli.verbose {
-//         //std::env::set_var("RUST_LOG", "debug");
-//         env_logger::init();
-//     }
-
-//     ctrlc::set_handler(move || {
-//         println!("Received Ctrl+C! Gracefully shutting down...");
-//         exit(CliantError::UserCancelled.exit_code());
-//     })
-//     .expect("Error setting Ctrl-C handler");
-
-//     //let url = Url::parse(&cli.url)?;
-//     //remember to improve cli to accept multiple arguments.
-
-//     sync_start(cli.url.clone(), cli)?;
-
-//     Ok(())
-// }
-
-// fn sync_start(url: Url, cli: Cli) -> Result<(), CliantError> {
-//     let client: reqwest::blocking::Client = HttpClientConfig::new(
-//         cli.username,
-//         cli.password,
-//         cli.max_redirects,
-//         cli.timeout,
-//         cli.proxy_url,
-//         cli.request_headers,
-//         cli.http_cookies,
-//         cli.http_version,
-//     )
-//     .try_into()?;
-//     let download_manager = sync::download_manager::DownloadManager::new(
-//         vec![url],
-//         client,
-//         cli.max_concurrent_part,
-//         3,
-//         cli.path,
-//         sync::DiskFileSystem,
-//     )?;
-//     download_manager.start_tasks()?;
-//     Ok(())
-// }
-fn main() {
-    let _ = Cliant::parse();
+#[tokio::main]
+async fn main()->Result<()>{
+    human_panic::setup_panic!();
+    set_up_cli_app().await?;
+    Ok(())
 }
