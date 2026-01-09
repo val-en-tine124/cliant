c5f2bcb74782c453de24e008b8b95b9abc004640
val-en-tine124 <valentinechibueze400@gmail.com>
2026-01-09 15:18:18 +0100

    chore: Remove allow unused fpr DataTransport

--- src/shared/network/mod.rs
+++ src/shared/network/mod.rs
@@ -3,11 +3,10 @@ use anyhow::Result;
 use tokio_stream::Stream;
 
 use crate::shared::errors::CliantError;
-mod http_args;
+pub mod http_args;
 mod http;
 pub mod factory;
 
-#[allow(unused)]
 pub trait DataTransport:Send+Sync{
     async fn receive_data(&self,source:url::Url) -> Result<impl Stream<Item = Result<Bytes,CliantError>>,CliantError>;
 }
