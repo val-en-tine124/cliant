a1a1ac0d45823fb48576188e28699586525c2a0c
val-en-tine124 <valentinechibueze400@gmail.com>
2026-01-10 12:26:48 +0100

    chore: add handler to save_to_local

--- src/features/save_to_local/handler.rs
+++ src/features/save_to_local/handler.rs
@@ -1,19 +1,56 @@
 use anyhow::Result;
-use crate::shared::network::http_args::HttpArgs;
+use tracing::{error,trace,info};
+use url::Url;
+use tokio_stream::StreamExt;
+use crate::shared::{fs::FsOps, network::http_args::HttpArgs};
 use super::cli::LocalArgs;
-use crate::shared::network::factory::{TransportType,handle_http};
-use crate::shared::fs::local::{LocalFs,LocalFsBuilder};
+use crate::shared::network::{factory::{TransportType,handle_http},DataTransport};
+use crate::shared::fs::local::LocalFsBuilder;
 
-pub async fn handle(args:LocalArgs,http_args:HttpArgs)->Result<()>{
+pub async fn handle(url:Url,args:LocalArgs,http_args:HttpArgs)->Result<()>{
     let file_path=args.output;
     let file_parent_dir=file_path.ancestors().next().unwrap().to_path_buf();
     let transport=match args.transport{
         TransportType::Http=>{
             handle_http(http_args,TransportType::Http)
         }
-    };
-    let builder=LocalFsBuilder::new().path(file_path).root_path(file_parent_dir).build().await?;
+    }?;
+    let builder=LocalFsBuilder::new().path(file_path.clone()).root_path(file_parent_dir).build().await?;
     
-    Ok(())
+    let stream_result=transport.receive_data(url.clone()).await;
+    match stream_result{
+        Ok( mut stream)=>{
+            loop{
+                
+                match stream.try_next().await {
+                    Ok(Some(bytes))=>{
+                        let bytes_size=std::mem::size_of_val(&bytes);
+                        trace!("Writing bytes of size {} to {file_path:?}",bytes_size);
+                        builder.append_bytes(bytes).await?;
 
-}
+                    }
+
+                    Ok(None)=>{
+                        info!("Reach the EOF,streaming completed.");
+                        builder.close_fs().await;
+                        break;
+                    }
+                    Err(err)=>{
+                        error!("Can't get next item on network stream,caused by:{err}");
+                        builder.close_fs().await;
+                        return Err(err.into());
+
+                    }
+                }
+            }
+            }   
+
+        Err(err)=>{
+            error!("Can't get {url} data, caused by:{err}");
+        }
+
+        }
+        
+    Ok(())
+    }
+
