19a886423af001101fd181a4bd7686481b2f218f
val-en-tine124 <valentinechibueze400@gmail.com>
2026-01-12 20:47:20 +0100

    chore: Add secrecy crate for obfuscating password and zerorizing  in memory andremove unnessary code  build_client_impl

--- src/shared/network/http/config.rs
+++ src/shared/network/http/config.rs
@@ -1,15 +1,15 @@
 use anyhow::{Error as AnyhowError, Result};
 use cookie::Cookie;
 use derive_getters::Getters;
-use reqwest::blocking::{Client, ClientBuilder};
+use reqwest::{Client, ClientBuilder};
 use reqwest::header::{COOKIE, HeaderMap, HeaderValue};
 use reqwest::{Proxy, redirect::Policy};
-use serde::Deserialize;
+use secrecy::{SecretString,ExposeSecret,};
 use std::str::FromStr;
 use std::time::Duration;
 use tracing::{error, info, warn};
 use clap::{command,Args,arg};
-#[derive(Debug,Deserialize,Args, Getters, Clone, Copy)]
+#[derive(Debug,Args, Getters, Clone, Copy)]
 pub struct RetryArgs {
     ///This is the maximum number of http request 
     /// retries that will be made to server incase a network issue occur.
@@ -33,7 +33,7 @@ impl Default for RetryArgs {
     }
 }
 
-#[derive(Deserialize,Args,Debug, Clone)]
+#[derive(Args,Debug, Clone)]
 pub struct HttpArgs {
     #[command(flatten)]
     pub retry_args:Option<RetryArgs>,
@@ -42,7 +42,7 @@ pub struct HttpArgs {
     pub username: Option<String>,
     /// Set http basic authentication password to used for login to the site.
     #[arg(short='P',long)]
-    pub password: Option<String>,
+    pub password: Option<SecretString>,
     ///Maximum http redirects this application will make if need be.
     pub max_redirects: Option<usize>,
     /// Set http timeout(in secs) for all http request.
@@ -75,16 +75,6 @@ impl Default for HttpArgs {
     }
 }
 
-
-impl TryFrom<HttpArgs> for Client {
-    type Error = AnyhowError;
-
-    /// Tries to convert an `HttpArgs` into a `reqwest::blocking::Client`.
-    fn try_from(http_config: HttpArgs) -> Result<Self, Self::Error> {
-        build_client(http_config)
-    }
-}
-
 impl TryFrom<HttpArgs> for reqwest::Client {
     type Error = AnyhowError;
 
@@ -94,97 +84,85 @@ impl TryFrom<HttpArgs> for reqwest::Client {
     }
 }
 
-macro_rules! build_client_impl {
-    ($builder:ty, $client:ty) => {
-        fn build_client_base(http_config: HttpArgs) -> Result<$client, AnyhowError> {
-            let mut client_config = <$builder>::new();
-            info!("Initialized client builder.");
 
-            let policy: Policy = if let Some(max_redirects) = http_config.max_redirects {
-                info!("Maximum redirect has been set to {}", max_redirects);
-                Policy::limited(max_redirects)
-            } else {
-                info!("Maximum redirect still Cliant default");
-                Policy::default()
-            };
+    fn build_client_base(http_config: HttpArgs) -> Result<Client, AnyhowError> {
+        let mut client_config = ClientBuilder::new();
+        info!("Initialized client builder.");
 
-            let timeout = {
-                info!("Setting timeout to {}.", http_config.timeout);
-                Duration::from_secs(http_config.timeout as u64)
-            };
+        let policy: Policy = if let Some(max_redirects) = http_config.max_redirects {
+            info!("Maximum redirect has been set to {}", max_redirects);
+            Policy::limited(max_redirects)
+        } else {
+            info!("Maximum redirect still Cliant default");
+            Policy::default()
+        };
 
-            client_config = client_config.timeout(timeout).redirect(policy);
+        let timeout = {
+            info!("Setting timeout to {}.", http_config.timeout);
+            Duration::from_secs(http_config.timeout as u64)
+        };
 
-            if let Some(proxy_url) = http_config.proxy_url {
-                info!("Setting up user-defined proxy for Cliant");
-                client_config = client_config.proxy(Proxy::all(proxy_url)?);
-            } else {
-                info!("No user defined proxy.");
-                client_config = client_config.no_proxy();
-            }
+        client_config = client_config.timeout(timeout).redirect(policy);
 
-            if let Some(http_version) = http_config.http_version {
-                client_config = match http_version.as_str() {
-                    "1.1" => {
-                        info!("Still HTTP version 1.1.");
-                        client_config.http1_only()
-                    }
-
-                    _ => {
-                        warn!("Unsupported http version, using default http version 1.1.");
-                        client_config.http1_only()
-                    }
-                }
-            }
-
-            let mut request_header_headermap = HeaderMap::new();
-            // comma seperated header value e.g name:johndoe,age:23
-            if let Some(request_headers_str) = http_config.request_headers {
-                info!("Setting up user-defined HTTP headers.");
-                for header in request_headers_str.split(',').map(|s| s.trim()) {
-                    let parts: Vec<&str> = header.splitn(2, ':').collect();
-                    if parts.len() == 2 {
-                        let name = parts[0].trim();
-                        let value = parts[1].trim();
-                        request_header_headermap.insert(
-                            reqwest::header::HeaderName::from_str(name)?,
-                            HeaderValue::from_str(value)?,
-                        );
-                    }
-                }
-            }
-
-            if let Some(cookies_str) = http_config.http_cookies {
-                info!("Setting up user-defined HTTP cookies.");
-                match Cookie::parse(cookies_str) {
-                    Ok(cookie) => {
-                        request_header_headermap
-                            .insert(COOKIE, HeaderValue::from_str(cookie.to_string().as_ref())?);
-                    }
-                    Err(err) => {
-                        error!(error = %err, "Can't sanitize cookie");
-                    }
-                }
-            }
-
-            let client = client_config
-                .default_headers(request_header_headermap)
-                .build()?;
-            info!("Built HTTP client with User configuration");
-
-            Ok(client)
+        if let Some(proxy_url) = http_config.proxy_url {
+            info!("Setting up user-defined proxy for Cliant");
+            client_config = client_config.proxy(Proxy::all(proxy_url)?);
+        } else {
+            info!("No user defined proxy.");
+            client_config = client_config.no_proxy();
         }
-    };
-}
 
-fn build_client(http_config: HttpArgs) -> Result<Client, AnyhowError> {
-    build_client_impl!(ClientBuilder, Client);
-    build_client_base(http_config)
-}
+        if let Some(http_version) = http_config.http_version {
+            client_config = if http_version.as_str() == "1.1" {
+                info!("Still HTTP version 1.1.");
+                client_config.http1_only()
+            } else {
+                warn!("Unsupported http version, using default http version 1.1.");
+                client_config.http1_only()
+            }
+        }
+
+        let mut request_header_headermap = HeaderMap::new();
+        // comma seperated header value e.g name:johndoe,age:23
+        if let Some(request_headers_str) = http_config.request_headers {
+            info!("Setting up user-defined HTTP headers.");
+            for header in request_headers_str.split(',').map(str::trim) {
+                let parts: Vec<&str> = header.splitn(2, ':').collect();
+                if parts.len() == 2 {
+                    let name = parts[0].trim();
+                    let value = parts[1].trim();
+                    request_header_headermap.insert(
+                        reqwest::header::HeaderName::from_str(name)?,
+                        HeaderValue::from_str(value)?,
+                    );
+                }
+            }
+        }
+
+        if let Some(cookies_str) = http_config.http_cookies {
+            info!("Setting up user-defined HTTP cookies.");
+            match Cookie::parse(cookies_str) {
+                Ok(cookie) => {
+                    request_header_headermap
+                        .insert(COOKIE, HeaderValue::from_str(cookie.to_string().as_ref())?);
+                }
+                Err(err) => {
+                    error!(error = %err, "Can't sanitize cookie");
+                }
+            }
+        }
+
+        let client = client_config
+            .default_headers(request_header_headermap)
+            .build()?;
+        info!("Built HTTP client with User configuration");
+        Ok(client)
+    }
+
+
 
 fn build_async_client(
     http_config: HttpArgs,
 ) -> Result<reqwest::Client, AnyhowError> {
-    build_client_impl!(reqwest::ClientBuilder, reqwest::Client);
     build_client_base(http_config)
 }
